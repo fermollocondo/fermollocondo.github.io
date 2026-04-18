752977ed35af593bb3b8842e803e189ac335a478
Fernanda L. Mollocondo <fernanda.mollocondo@gmail.com>
2026-04-18 15:27:25 -0600

    Actualizar sección de about y portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -11,13 +11,13 @@ export const PROJECTS_DATA = [
   {
     title: 'Colour Systems 1',
     description: 'An exploration of color theory, semantic relationships, and structured palettes to build intentional visual systems.',
-    link: 'https://www.behance.net/gallery/172503633/Colour-Systems-1?tracking_source=project_owner_other_projects',
+    // link: 'https://www.behance.net/gallery/172503633/Colour-Systems-1?tracking_source=project_owner_other_projects',
     tech: 'Color Theory / Visual Systems'
   },
   {
     title: 'Cabinet Philosophical Salon',
     description: 'A dedicated space—both conceptual and physical—for rigorous thought, creative exploration, and multidisciplinary dialogue.',
-    link: 'https://www.behance.net/gallery/209720615/Cabinet-Philosophical-Salon',
+    // link: 'https://www.behance.net/gallery/209720615/Cabinet-Philosophical-Salon',
     tech: 'Branding / Typography / Visual Systems'
   },
 ];
